1a20cd6346c059ad9f92692d1c3606e1312b66da
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-14 18:23:53 -0400

    localstorage to get value

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,4 +12,18 @@ $("#currentDay").text(today)
         console.log(textInput);
 })
 
+//checks if any values in textInput, if none create an array to store values
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("textInput")) || [];
+
+  
+ for (var i=0; i< tasks.list; i++){
+     $('textInput').append(tasks);
+ }
+  };
+  
+  var saveTasks = function() {
+    localStorage.setItem("textInput", JSON.stringify(tasks));
+  };
+
 

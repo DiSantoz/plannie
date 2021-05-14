7fb2028d21fe34ab5b54117b7dba045bf7dfb8e6
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-13 22:13:10 -0400

    grab input value from user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,12 @@ var today = moment().format("dddd, MMMM do");
 
 $("#currentDay").text(today)
 
+// grab value of user text from planner
+
+    $(".saveBtn").on("click", function(){
+        var textInput = $(".description").val()
+        
+        console.log(textInput);
+})
+
+

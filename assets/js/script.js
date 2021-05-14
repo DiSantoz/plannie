8fa966bcf77cdbe76cf9eaad7528d06d80c94698
David Ferreira <dferreira@MacBook-Pro.fios-router.home>
2021-05-14 19:32:38 -0400

    save task and time to local storage on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,17 @@ $("#currentDay").text(today)
 // grab value of user text from planner
 
     $(".saveBtn").on("click", function(){
+        var timeInput = $(this).parent().prev().prev().children("p").text()
         var textInput = $(this).parent().prev().children("textarea").val()
         
+        console.log(timeInput);
         console.log(textInput);
-})
 
-//checks if any values in textInput, if none create an array to store values
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("textInput")) || [];
+// if time and text both have values then save to localStorage
+if (timeInput && textInput){
+    localStorage.setItem(timeInput, textInput);
 
-  
- for (var i=0; i< tasks.list; i++){
-     $('textInput').append(tasks);
- }
-  };
-  
-  var saveTasks = function() {
-    localStorage.setItem("textInput", JSON.stringify(tasks));
-  };
+}
 
+ })
 
